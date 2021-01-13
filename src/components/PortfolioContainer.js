df6e987fdf97286ac3d76b132bd254b498848d43
Caspento <Mrcaspento@gmail.com>
2021-01-13 18:42:36 -0500

    creating a portfolio container which will be the container for the entire website that has links to each tab list

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Home from "../pages/Home";
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Projects from '../pages/Projects';
+
+
+class Portfolio extends Component {
+
+state = {
+    currentPage: 'Home'
+};
+
+handlePageChange = page => {
+    this.setState({ currentPage: page});
+}
+}
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/index';
+import Footer from '../components/Footer/index';
+import NavBar from '../components/NavBar';
+import Card from "../components/Card";
+
+
+const About = () => (
+
+
+
+    
+)
