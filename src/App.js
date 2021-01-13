58828c291db046cf9a9dfc8f0aa8cad723e4ea62
Caspento <Mrcaspento@gmail.com>
2021-01-13 18:10:05 -0500

    creating the NavBar and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Wrapper from './components/Wrapper/index';
+import Footer from './components/Footer/index';
+import About from './pages/About';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <NavBar />
+      <Wrapper>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
+      </Wrapper>
+      <Footer />
     </div>
+  </Router>
   );
 }
 
