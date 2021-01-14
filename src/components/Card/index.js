f06ef9ac0a097a0691c026c045fd888073946563
Caspento <Mrcaspento@gmail.com>
2021-01-13 19:12:41 -0500

    almost scoping myself trying to now work on each route && prop

---
+++ src/components/Card/index.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import CardBody from '../CardBody/index'
---
+++ src/components/CardTitle/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CardTitleText from "../CardTitleText";
+import "./style.css";
+
+function CardTitle() {
+  return (
+    <div className="heading">
+      <h1>About me</h1>
+    </div>
+  );
+}
+
+export default CardTitle;
